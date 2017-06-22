37b840d605bc5c16def1f89057df9dc9c15a7c77
gmlove <gracekinglau@gmail.com>
2017-06-22 14:13:43 +0800

    improve test for dataset

--- week9/dataset_test.py
+++ week9/dataset_test.py
@@ -13,6 +13,8 @@ class GANDatasetTest(tf.test.TestCase):
         self.assertEqual(dataset.next_batch().shape, (1, 28, 28, 1))
         self.assertTrue(dataset.has_more_than(1))
         self.assertFalse(dataset.has_more_than(2))
+        dataset.reset()
+        self.assertTrue(dataset.has_more_than(2))
 
 
 if __name__ == '__main__':
